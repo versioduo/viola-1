3649e0efcae198549c9c4068c37a30aceff6b4de
Kay Sievers <kay@versioduo.com>
2026-05-05 16:18:12 +0200

    Decrease finger acceleration

--- viola-1.ino
+++ viola-1.ino
@@ -9,7 +9,7 @@
 #include <V2PowerSupply.h>
 #include <V2Stepper.h>
 
-V2DEVICE_METADATA("com.versioduo.viola-1", 52, "versioduo:samd:step");
+V2DEVICE_METADATA("com.versioduo.viola-1", 53, "versioduo:samd:step");
 
 namespace {
   constexpr uint8_t       notesMax{20};
@@ -112,10 +112,10 @@ namespace {
       Stepper::Pressure),
     Stepper(
       {
-        .ampere{0.8},
+        .ampere{0.7},
         .microstepsShift{4},
         .home{.speed{750}, .stall{0.04}},
-        .speed{.min{50}, .max{2400}, .accel{20000}},
+        .speed{.min{50}, .max{2400}, .accel{16000}},
       },
       Stepper::Finger),
     Stepper(
