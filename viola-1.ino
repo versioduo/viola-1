ff9f8222e0a1e93052afc664b01f7afe551630ef
Kay Sievers <kay@versioduo.com>
2026-05-05 15:22:41 +0200

    Increase finger power

--- viola-1.ino
+++ viola-1.ino
@@ -9,7 +9,7 @@
 #include <V2PowerSupply.h>
 #include <V2Stepper.h>
 
-V2DEVICE_METADATA("com.versioduo.viola-1", 50, "versioduo:samd:step");
+V2DEVICE_METADATA("com.versioduo.viola-1", 51, "versioduo:samd:step");
 
 namespace {
   constexpr uint8_t       notesMax{20};
@@ -112,7 +112,7 @@ namespace {
       Stepper::Pressure),
     Stepper(
       {
-        .ampere{0.7},
+        .ampere{0.8},
         .microstepsShift{4},
         .home{.speed{750}, .stall{0.04}},
         .speed{.min{50}, .max{2400}, .accel{24000}},
