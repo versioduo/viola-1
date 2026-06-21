553642fd13ae23f480d5a8947222311cb70bbd44
Kay Sievers <kay@versioduo.com>
2026-06-21 21:55:47 +0200

    Update MIDI API

--- viola-1.ino
+++ viola-1.ino
@@ -1235,7 +1235,7 @@ namespace {
     // Receive a host event from our parent device
     auto receivePlug(V2Link::Packet* packet) -> void override {
       if (packet->getType() == V2Link::Packet::Type::MIDI) {
-        packet->receive(&_midi);
+        packet->copyTo(_midi);
         Device.dispatch(&Plug, &_midi);
       }
     }
@@ -1248,7 +1248,7 @@ namespace {
           return;
 
         if (Device.usb.midi.connected()) {
-          packet->receive(&_midi);
+          packet->copyTo(_midi);
           _midi.setPort(address + 1);
           Device.usb.midi.send(&_midi);
         }
